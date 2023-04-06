efec254f31adb33c88a88acb56dacfa4d57a84b4
Dorian GARCIA <93933273+Dorian-Garcia-pro@users.noreply.github.com>
2023-04-07 01:51:36 +0900

    filters

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,35 @@
 import React from "react";
-import './Card.scss';
-import { useSelector, useDispatch } from 'react-redux';
+import "./Card.scss";
+import { useSelector, useDispatch } from "react-redux";
 import { flipCard } from "../cardSlice.js";
 
-function Card({ front, back, rndRectoVerso}) {
-  const  cardStore  = useSelector((store) => store.card)
-  const dispatch = useDispatch()
+function Card({ front, back, rndRectoVerso }) {
+  const cardStore = useSelector((store) => store.card);
+  const dispatch = useDispatch();
 
-  const handleClick = () => {
-   dispatch(flipCard())
+  const leFlip = () => {
+    dispatch(flipCard());
   };
 
+  let chosneNb = cardStore.listHiraganas.filter((e) => e.selected === true)[
+    cardStore.InitNbCard
+  ]
+    ? cardStore.listHiraganas.filter((e) => e.selected === true)[
+        cardStore.InitNbCard
+      ]
+    : cardStore.listHiraganas[0];
+
   return (
-    <div className={`card ${cardStore.rectoVerso ? "" : "flipped"}`} onClick={handleClick}>
-      <div className="front"><p>{front}</p></div>
-      <div className="back"><p>{back}</p></div>
+    <div
+      className={`card ${cardStore.rectoVerso ? "" : "flipped"}`}
+      onClick={leFlip}
+    >
+      <div className="front">
+        <p>{chosneNb.kana}</p>
+      </div>
+      <div className="back">
+        <p>{chosneNb.romaji}</p>
+      </div>
     </div>
   );
 }
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,44 +1,101 @@
 import Card from "./Card.js";
-import { useSelector, useDispatch } from 'react-redux'; 
-import { rndCard,rndRectoVerso, flipCard, invertInfinite, resetToRecto} from "../cardSlice.js";
+import "./Deck.scss";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  rndCard,
+  flipCard,
+  resetToRecto,
+  selectedToggleKanaFilter,
+  selectedToggleAll,
+  togglefilters,
+} from "../cardSlice.js";
 
 function Deck({ front, back }) {
-  const  cardStore  = useSelector((store) => store.card)
-  const dispatch = useDispatch()
-
-  const handleAll = () => {
-    dispatch(rndCard())
-    dispatch(rndRectoVerso())
-  }
+  const cardStore = useSelector((store) => store.card);
+  const dispatch = useDispatch();
 
   function handleRndInvertInfinite() {
-    dispatch(rndCard())
     dispatch(resetToRecto());
- 
+    setTimeout(() => {
+      dispatch(rndCard());
+    }, 120);
   }
 
-  const handleAllInfinite = () => {
-    cardStore.infiniteMode ? (
-    dispatch(rndCard())
-    ) :
-    dispatch(flipCard());
-    dispatch(invertInfinite())
-  }
+  const selectedInfinite = (kana) => {
+    cardStore.rectoVerso ? dispatch(flipCard()) : handleRndInvertInfinite();
+  };
 
-  const handleAllInfiniteOnlyRecto = () => {
-    cardStore.rectoVerso ? ( 
-      dispatch(flipCard())
-        ) :   
-    handleRndInvertInfinite();  
-  }
- 
-  return <div className="card-container"> 
-    <Card key={cardStore.listHiraganas[cardStore.InitNbCard].nb} front={cardStore.listHiraganas[cardStore.InitNbCard].kana} back={cardStore.listHiraganas[cardStore.InitNbCard].romaji} rndRectoVerso={cardStore.rectoVerso} />
-   <button onClick={() => handleAll()}>Full RNG</button>
-   <button onClick={() => handleAllInfinite()}>Full RNG - Infinite</button>
-   <button onClick={() => handleAllInfiniteOnlyRecto()}>InfiniteOnlyRecto</button>
-   <div className="selectKanas"></div>
-  </div>;
+  /*   const test = (kana) => {}; */
+
+  return (
+    <div className="deck-container">
+      <div className="leftCol">
+        <p>Ok</p>
+      </div>
+      <div className="card-container">
+        <Card />
+
+        <button
+          className="btn"
+          onClick={() =>
+            cardStore.listHiraganas.filter((e) => e.selected === true).length <=
+            1
+              ? dispatch(flipCard())
+              : selectedInfinite()
+          }
+        >
+          Selected Infinite
+        </button>
+      </div>
+
+      <div className="filtersContainer">
+        <button className="btn" onClick={() => dispatch(togglefilters())}>
+          Filtres
+        </button>
+        {cardStore.displayfilters ? (
+          <div className="allFilters">
+            <div className="allFiltersBtnToggleAll">
+              <button
+                className="btn"
+                onClick={() => dispatch(selectedToggleAll(true))}
+              >
+                Select All
+              </button>
+              <button
+                className="btn"
+                onClick={() => dispatch(selectedToggleAll(false))}
+              >
+                Unselect All
+              </button>
+            </div>
+            <div className="selectKanas">
+              {cardStore.listHiraganas
+                .filter((kana) => kana.cat !== "combinaisons")
+                .map((kana) =>
+                  kana.selected ? (
+                    <p
+                      key={kana.nb}
+                      className="kanaSelected"
+                      onClick={() => dispatch(selectedToggleKanaFilter(kana))}
+                    >
+                      {kana.kana}
+                    </p>
+                  ) : (
+                    <p
+                      key={kana.nb}
+                      className=""
+                      onClick={() => dispatch(selectedToggleKanaFilter(kana))}
+                    >
+                      {kana.kana}
+                    </p>
+                  )
+                )}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 export default Deck;
