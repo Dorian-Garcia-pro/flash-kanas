ab5f5c879fc60b5017859a5babfef1346b86fcfe
Dorian GARCIA <93933273+Dorian-Garcia-pro@users.noreply.github.com>
2023-05-07 14:52:59 +0900

    reformule GramPause

--- src/components/GramPauses.js
+++ src/components/GramPauses.js
@@ -5,21 +5,36 @@ import {} from "../cardSlice.js";
 function GramPauses() {
   return (
     <div className="containerDigrammes">
-      <div className="digrammesExplication">
-        <p>
-          La langue japonaise utilise parfois une pause pour marquer un temps
-          d'arrêt juste avant une consonne dans un mot. Cette pause a une durée
-          équivalente à celle d'une syllabe normale et s'applique aux hiraganas
-          et aux katakanas. En romaji, cette pause est notée en doublant la
-          consonne qui précède le son, tandis qu'en kana, on utilise un petit
-          "tsu" (っ en hiragana et ッ en katakana). La pause est lue en marquant
-          un temps d'arrêt pour les sons T, K et P, tandis que pour les sons S
-          et Z, elle prolonge le son. Il est important de noter que la pause
-          change le mot et que la prononciation est cruciale, en particulier
-          dans les conjugaisons de verbes où elle est souvent utilisée.{" "}
-          <br></br>Par exemple, "した" signifie "je faisais", tandis que
-          "しった" signifie "je savais".
-        </p>
+      <div id="pausesExplication" className="digrammesExplication">
+        <article>
+          <p>
+            En japonaise on utilise parfois une pause pour marquer un temps
+            d'arrêt juste avant une consonne dans un mot.
+          </p>
+          <p>
+            Cette pause a une durée équivalente à celle d'une syllabe normale et
+            s'applique aux hiraganas et aux katakanas.
+          </p>
+          <p>
+            En romaji, cette pause est notée en doublant la consonne qui précède
+            le son, tandis qu'en kana, on utilise un petit "tsu".「っ」 en
+            hiragana et 「ッ」 en katakana.
+          </p>
+          <p>
+            La pause est lue en marquant un temps d'arrêt pour les sons T, K et
+            P, tandis que pour les sons S et Z, elle prolonge le son.
+          </p>
+          <p>
+            Il est important de noter que la pause change le mot et que la
+            prononciation est cruciale, en particulier dans les conjugaisons de
+            verbes où elle est souvent utilisée.
+          </p>
+          <p>
+            Par exemple, 「した」(shita) signifie "je faisais", tandis que 「し
+            <span style={{ color: "red" }}>っ</span>た」(shitta) signifie "je
+            savais".
+          </p>
+        </article>
         <p>Exemple :</p>
         <p>
           <ul>
